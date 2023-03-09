27400e78e7279cbfded761de0a96845c07ad1da2
TAN0N <at3280@student.uni-lj.si>
2023-03-09 13:24:56 +0100

    Add project files.

---
+++ View/BannerHelper.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TOS_Helper.View
+{
+    /// <summary>
+    /// Interaction logic for BannerHelper.xaml
+    /// </summary>
+    public partial class BannerHelper : Window
+    {
+        public BannerHelper()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
